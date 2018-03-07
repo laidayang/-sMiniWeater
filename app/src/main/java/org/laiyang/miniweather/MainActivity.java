9c9ea7303f5c8f979f09281b0c230a22e3080472
laidayang <laiyangs@foxmail.com>
2018-03-07 17:01:55 +0800

    init project

---
+++ app/src/main/java/org/laiyang/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package org.laiyang.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 小米笔记本Pro on 2018/3/7.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
