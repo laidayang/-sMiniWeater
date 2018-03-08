80294687c1ec73d12a4038fcbb78948048f4446a
laidayang <laiyangs@foxmail.com>
2018-03-08 20:53:34 +0800

    init project

--- app/src/main/java/org/laiyang/util/Netutil.java
+++ app/src/main/java/org/laiyang/util/Netutil.java
@@ -4,5 +4,26 @@ package org.laiyang.util;
  * Created by 小米笔记本Pro on 2018/3/8.
  */
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 public class Netutil {
+    public static final int NETWOEN_NONE = 0;
+    public static final int NETWORN_WIFI = 1;
+    public static final int NETWORN_MOBILE = 2;
+    public static int getNetworkState(Context context){
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+       NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+       if(networkInfo == null){
+           return  NETWOEN_NONE;
+       }
+       int nType = networkInfo.getType();
+       if(nType == ConnectivityManager.TYPE_MOBILE){
+           return NETWORN_MOBILE;
+       }
+       else if(nType == ConnectivityManager.TYPE_WIFI){
+           return NETWORN_WIFI;
+       }
+       return NETWOEN_NONE;
+    }
 }
